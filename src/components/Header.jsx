22d528b961270dcfba5938f81494e5e60449e3c6
Sumeya <sumeyaibrahim72@gmail.com>
2023-06-05 14:33:50 +0300

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>todos</h1>
+      <p>Items will persist in the browser local storage</p>
+    </header>
+  );
+}
